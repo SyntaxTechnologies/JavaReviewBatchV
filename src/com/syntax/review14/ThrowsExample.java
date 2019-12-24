c854bb898860b5d4587de2d1c08f860f9e68ad57
Elion Limanaj <elionlimanaj@Elions-MacBook-Pro.local>
2019-12-23 21:31:21 -0500

    Throws and Throw

---
+++ src/com/syntax/review14/ThrowsExample.java
@@ -0,0 +1,29 @@
+package com.syntax.review14;
+
+public class ThrowsExample {
+
+	public static void main(String[] args) {
+		try {
+			int res = divide(10, 0);
+			System.out.println("result is " + res);
+		} catch (Exception ex) {
+			System.out.println("Division by zero in Main method");
+			System.out.println(ex);
+		}
+
+		System.out.println("Happy Ending");
+	}
+
+	public static int divide(int a, int b) throws Exception {
+		int result = 0;
+		
+		try {
+			result = a / b;
+		} catch (Exception e) {
+			System.out.println("Division by zero in Divide method");
+			throw e;
+		}
+		
+		return result;
+	}
+}
